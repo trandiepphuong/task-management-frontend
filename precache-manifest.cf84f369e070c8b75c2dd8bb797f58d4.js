d37322be7224ff2ab1ec75e8a4b966b686743eb3
trandiepphuong <trandiepphuong@users.noreply.github.com>
2021-11-01 04:56:50 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.cf84f369e070c8b75c2dd8bb797f58d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d09202bd4d0da3ec84725f45e6e2fd0",
+    "url": "htpps://trandiepphuong.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "9eb8a8abefa016af6250",
+    "url": "htpps://trandiepphuong.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "4299fbf30b4c4d6f8829",
+    "url": "htpps://trandiepphuong.github.io/task-management-frontend/static/js/2.5723e681.chunk.js"
+  },
+  {
+    "revision": "9eb8a8abefa016af6250",
+    "url": "htpps://trandiepphuong.github.io/task-management-frontend/static/js/main.604cdcd1.chunk.js"
+  },
+  {
+    "revision": "ee6e3002e0a2c8dbd1ca",
+    "url": "htpps://trandiepphuong.github.io/task-management-frontend/static/js/runtime~main.731274bc.js"
+  }
+]);
